47215771dd37a4ced631a589fa6ab6235c63a2a6
Jeetendra Gund <jsgund@gmail.com>
2019-11-03 19:47:17 +0530

    Db table screen shot

--- ShadowPropertyInEFCore/ShadowPropertyInEFCore/Program.cs
+++ ShadowPropertyInEFCore/ShadowPropertyInEFCore/Program.cs
@@ -14,15 +14,20 @@ namespace ShadowPropertyInEFCore
                     //Add record
                     db.Shadows.Add(new Shadow()
                     {
-                        Content = "Test",
+                        Content = "Shadow Property",
                         CreatedDate = DateTime.Now
                     });
 
                     db.SaveChanges();
                     
                     var res = db.Shadows.FirstOrDefault();
-                    
-                    Console.WriteLine("Hello World!");
+
+                    if (res != null)
+                    {
+                        Console.WriteLine("Added record Id :- {0}", res.Id);
+                        Console.WriteLine("Added record Content :- {0}", res.Content);
+                        Console.WriteLine("Added record CreatedDate - {0}", res.CreatedDate);
+                    }
                 }
             }
             catch (Exception e)
